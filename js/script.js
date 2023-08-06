f36bcbbe0530fc37b8f60b784d2d4b9a54dd3e26
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-08-06 10:47:40 -0500

    iserindo função da borracha

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ let brushSize=20
 
 let isPainting = false
 
+let activeTool = "brush"
 
 //escolha a cor
 
@@ -21,25 +22,54 @@ inputColor.addEventListener("change", ({target}) => {
 
 //evento para desenha com mouse
 canvas.addEventListener("mousedown", ({clientX, clientY}) => {
-    draw(clientX, clientY)
     isPainting = true
+    if (activeTool == "brush") {
+        draw(clientX, clientY)
+    }
 })
 
 canvas.addEventListener("mousemove", ({clientX, clientY}) => {
     if(isPainting) {
-        draw(clientX, clientY)
+        if (activeTool == "brush") {
+            draw(clientX, clientY)
+        }
+        if(activeTool == "rubber"){
+            erase(clientX, clientY)
+        }
+        
     }
 })
+
 canvas.addEventListener("mouseup", ({clientX, clientY}) => {
     isPainting = false
 })
 
 
 const draw = (x, y) => {
+    ctx.globalCompositeOperation = "source-over"
     ctx.beginPath()
-
     ctx.arc(x - canvas.offsetLeft, y- canvas.offsetTop, brushSize / 2, 0, 2 * Math.PI)
     ctx.fill()
-
 }
 
+const erase = (x, y) => {
+    ctx.globalCompositeOperation = "destination-out"
+    ctx.beginPath()
+    ctx.arc(x - canvas.offsetLeft, y- canvas.offsetTop, brushSize / 2, 0, 2 * Math.PI)
+    ctx.fill()
+}
+
+const selectTool = ({target}) => {
+    const selectTool = target.closest("button")
+    const action = selectTool.getAttribute("data-action")
+
+    if(action) {
+        activeTool = action
+    }
+    
+   
+}
+
+tools.forEach((tool) => {
+    tool.addEventListener("click", selectTool)
+})
